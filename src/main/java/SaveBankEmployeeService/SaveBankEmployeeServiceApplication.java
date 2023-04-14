1bc47e9e7749ec80dc335472aaecace4a77328be
Lwazi <lwazigxogxa@gmail.com>
2023-04-14 09:16:07 +0200

    first commit

---
+++ src/main/java/SaveBankEmployeeService/SaveBankEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package SaveBankEmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBankEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveBankEmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SaveBankEmployeeService/SaveBankEmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SaveBankEmployeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveBankEmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
