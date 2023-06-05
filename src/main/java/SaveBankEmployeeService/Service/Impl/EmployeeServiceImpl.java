cebcaf55d3d44e791849e69fb15633889d3ee593
Lwazi <lwazigxogxa@gmail.com>
2023-06-05 15:31:10 +0200

    implement circuit breaker and retry

--- src/main/java/SaveBankEmployeeService/Service/Impl/EmployeeServiceImpl.java
+++ src/main/java/SaveBankEmployeeService/Service/Impl/EmployeeServiceImpl.java
@@ -10,7 +10,11 @@ import SaveBankEmployeeService.Mapper.EmployeeMapper;
 import SaveBankEmployeeService.Repository.EmployeeRepository;
 import SaveBankEmployeeService.Service.APIClient;
 import SaveBankEmployeeService.Service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +25,8 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl  implements EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     private EmployeeRepository employeeRepository;
 
@@ -44,15 +50,38 @@ public class EmployeeServiceImpl  implements EmployeeService {
         return savedEmployeeDto;
     }
 
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long id){
+
+        LOGGER.info("inside getEmployeeById() method");
 
         Employee employee = employeeRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("employee","id", id)
         );
         EmployeeDto savedEmployeeDto = EmployeeMapper.mapper.mapToEmployeeDto(employee);
 
-        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+       DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setDepartmentDto(departmentDto);
+        apiResponseDto.setEmployeeDto(savedEmployeeDto);
+
+        return apiResponseDto;
+    }
+
+    public APIResponseDto getDefaultDepartment(Long id, Exception exception){
+
+        LOGGER.info("inside getDefaultDepartment() method");
+
+        Employee employee = employeeRepository.findById(id).get();
+        EmployeeDto savedEmployeeDto = EmployeeMapper.mapper.mapToEmployeeDto(employee);
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("IT");
+        departmentDto.setDepartmentCode("DCIT");
+        departmentDto.setDepartmentDescription("IT department");
 
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setDepartmentDto(departmentDto);
